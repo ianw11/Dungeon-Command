b7273dae3107f5952e007f03947464de793069e7
Ian Washburne <ianw11@gmail.com>
2012-08-07 13:56:48 -0700

    Testing first push

---
+++ Dungeon Commander Mobile/src/washboard/dungeoncommandermobile/HomePage.java
@@ -0,0 +1,20 @@
+package washboard.dungeoncommandermobile;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+
+public class HomePage extends Activity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home_page);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.activity_home_page, menu);
+        return true;
+    }
+}
